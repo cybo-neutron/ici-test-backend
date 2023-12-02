b4b777c9fca8ac3df866c31ade4b65bf66b128a5
Niranjan <cyboneutron@gmail.com>
2023-12-02 09:08:48 +0530

    upload files complete

--- index.ts
+++ index.ts
@@ -15,7 +15,18 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.options("*", cors());
 
-const upload = multer({ dest: "uploads/" });
+// const upload = multer({ dest: "uploads/" });
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "./uploads");
+  },
+  filename: (req, file, cb) => {
+    console.log(file);
+    const fileName = `${Date.now()}-${file.originalname}`;
+    cb(null, fileName);
+  },
+});
+const upload = multer({ storage });
 
 app.get("/", async (req: Request, res: Response, next: NextFunction) => {
   res.json({
@@ -27,9 +38,9 @@ import uploadRoute from "@app/routes/v1/upload";
 app.use("/v1/upload", upload.array("files"), uploadRoute);
 
 app.post("/test", upload.array("files"), (req, res) => {
-  console.log("hello");
-  console.log(req.body);
-  console.log(req.files);
+  // console.log("hello");
+  // console.log(req.body);
+  // console.log(req.files);
   res.json({
     message: "got it",
   });
